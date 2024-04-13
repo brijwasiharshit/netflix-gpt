f541e1f5122a26fa75c8593884182124b8827008
brijwasiharshit <110799092+brijwasiharshit@users.noreply.github.com>
2024-04-13 07:49:08 +0530

    create-react-app and tailwindcss initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-5xl font-bold">
+      Netlix-GPT
+    </div>
+  );
+}
+
+export default App;
